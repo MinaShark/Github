9069780cb09909b87e7a0712f237fc5fc6afe18c
Mina Sediqi <mina@Minas-Air.fios-router.home>
2022-06-03 09:40:58 -0400

    changes added

--- src/gitHubClassProject/GitHubClass.java
+++ src/gitHubClassProject/GitHubClass.java
@@ -3,7 +3,10 @@ package gitHubClassProject;
 public class GitHubClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 30;
+		int b = 20;
+		int c = a+b;
+		System.out.println(c);
 
 	}
 
