d32102c322afdfe73f1fd0476cc54ecac9c692b2
MinaShark <105183275+MinaShark@users.noreply.github.com>
2022-06-03 18:27:29 -0400

    working

--- src/gitHubClassProject/NewClass.java
+++ src/gitHubClassProject/NewClass.java
@@ -6,4 +6,7 @@ public class NewClass {
 	// hi this class is not update yet in the remote repo
 	
 	// it did not work but now it is working..
+	
+	// i deleted a class. when it works it feels good
+	
 }
