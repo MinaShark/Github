152fac7abae57f33562eef2c014935af6a1132a4
MinaShark <105183275+MinaShark@users.noreply.github.com>
2022-06-03 15:24:04 -0400

    int added

--- src/gitHubClassProject/PracticeClass.java
+++ src/gitHubClassProject/PracticeClass.java
@@ -14,6 +14,9 @@ public class PracticeClass {
 		
 		// This is our new class and we brought some changes here. Why dont we see the changes?
 		// more changes. more edit
+		
+		int a = 10;
+		int b = 20;
 	}
 
 }
